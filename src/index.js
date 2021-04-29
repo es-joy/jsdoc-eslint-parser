d432830c80aab74377e7f1fc84b1976355020ed0
Brett Zamir <brettz9@yahoo.com>
2021-04-29 23:36:07 +0800

    - Add other expected deps.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@
 
 const babelParser = require('@babel/eslint-parser');
 
+const commentParser = require('comment-parser');
+
+const jsdoctypeparser = require('jsdoctypeparser');
+
 // eslint-disable-next-line no-console -- Testing
 console.log('babelParser', babelParser);
 
 // eslint-disable-next-line node/exports-style -- Required by ESLint
 exports.parseForESLint = function (code, options) {
   return {
-    // Todo: Add comments and comment types!
+    // Todo: Add comments and comment types! (ensure passing in comment option)
     ast: babelParser(code, options),
     // Expose comment or type parsing utilities?
     services: {
