b36040d031dfee65927840350ebc6fe827d22312
Brett Zamir <brettz9@yahoo.com>
2021-05-03 12:45:47 +0800

    - Begin utilizing more of jsdoccomment and esquery - npm: Bump deps. and devDeps

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@
 
 const {parseForESLint: babelParser} = require('@babel/eslint-parser');
 const commentParser = require('comment-parser');
-const jsdoctypeparser = require('jsdoctypeparser');
-const estraverse = require('estraverse');
-const {getJSDocComment} = require('@es-joy/jsdoccomment');
+const esquery = require('esquery');
+const {
+  getJSDocComment, jsdocVisitorKeys, jsdocTypeVisitorKeys,
+  commentParserToESTree, toCamelCase
+} = require('@es-joy/jsdoccomment');
 const {SourceCode} = require('eslint/lib/source-code/index.js');
 
 // eslint-disable-next-line node/exports-style -- Required by ESLint
@@ -22,31 +24,44 @@ exports.parseForESLint = function (code, options) {
   });
 
   // Todo: Add comments and comment types! (ensure passing in comment option)
-  // Todo: Add visitorKeys for comments and comment types both
+
   // Filed https://github.com/jsdoctypeparser/jsdoctypeparser/issues/139
   //  and commented at https://github.com/syavorsky/comment-parser/issues/99
   //  regarding jsdoctypeparser and comment-parser supporting this AST
   //  out of the box
-  commentParser;
-  jsdoctypeparser;
+
   // Todo: Need to utilize together with this `getJSDocComment`
   //  (ESLint's is deprecated). For now, might just traverse and run on
   //  each node (could use `estraverse`)
-  getJSDocComment;
   SourceCode;
-  estraverse;
+
+  const sel = esquery.parse('*[type]');
+  esquery.traverse(ast, sel, (node) => {
+    const {type} = node;
+    node.jsdoc = getJSDocComment(node);
+    node.type = `JSDocType${toCamelCase(type)}`;
+  });
+
+  const jsdoc = commentParser.parse(comment);
+  ast = commentParserToESTree(jsdoc, options.mode);
 
   console.log('ast', ast);
   return {
     ast,
     // Expose comment or type parsing utilities?
+    /*
     services: {
       foo () {
         // eslint-disable-next-line no-console -- Testing
         console.log('test');
       }
     },
+    */
     scopeManager,
-    visitorKeys
+    visitorKeys: {
+      ...visitorKeys,
+      ...jsdocVisitorKeys,
+      ...jsdocTypeVisitorKeys
+    }
   };
 };
